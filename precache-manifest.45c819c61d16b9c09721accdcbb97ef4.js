c7432cd850285a3ee879a88c6bae06008ff0eb86
Sayazrahman <115418700+Sayazrahman@users.noreply.github.com>
2022-11-23 07:52:31 -0800

    Add files via upload

---
+++ precache-manifest.45c819c61d16b9c09721accdcbb97ef4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c1b9d22570bd62f0392",
+    "url": "/Sayazrahman/News-world.com.git/static/css/main.fc261165.chunk.css"
+  },
+  {
+    "revision": "0c1b9d22570bd62f0392",
+    "url": "/Sayazrahman/News-world.com.git/static/js/main.0c1b9d22.chunk.js"
+  },
+  {
+    "revision": "fbee9149cc341e4ff9a0",
+    "url": "/Sayazrahman/News-world.com.git/static/js/1.fbee9149.chunk.js"
+  },
+  {
+    "revision": "90b3a98a706614ab63f8",
+    "url": "/Sayazrahman/News-world.com.git/static/js/2.90b3a98a.chunk.js"
+  },
+  {
+    "revision": "2af994c27332e244e4f2",
+    "url": "/Sayazrahman/News-world.com.git/static/js/runtime~main.2af994c2.js"
+  },
+  {
+    "revision": "444c0d9cf29d7ea460bff38539512cab",
+    "url": "/Sayazrahman/News-world.com.git/static/media/loading.444c0d9c.gif"
+  },
+  {
+    "revision": "d40d12fac541520932d25b87a29f3abd",
+    "url": "/Sayazrahman/News-world.com.git/index.html"
+  }
+];
