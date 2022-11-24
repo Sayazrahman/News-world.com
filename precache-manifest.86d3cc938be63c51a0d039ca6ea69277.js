eb098cf081584f4830947c7150e762cf316e01e7
paradoxayaz <rahmansyedayaz@gmail.com>
2022-11-23 21:29:08 -0800

    Updates

---
+++ precache-manifest.86d3cc938be63c51a0d039ca6ea69277.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c1b9d22570bd62f0392",
+    "url": "/News-world.com/static/css/main.fc261165.chunk.css"
+  },
+  {
+    "revision": "0c1b9d22570bd62f0392",
+    "url": "/News-world.com/static/js/main.0c1b9d22.chunk.js"
+  },
+  {
+    "revision": "fbee9149cc341e4ff9a0",
+    "url": "/News-world.com/static/js/1.fbee9149.chunk.js"
+  },
+  {
+    "revision": "90b3a98a706614ab63f8",
+    "url": "/News-world.com/static/js/2.90b3a98a.chunk.js"
+  },
+  {
+    "revision": "1db3c546d790b26a6119",
+    "url": "/News-world.com/static/js/runtime~main.1db3c546.js"
+  },
+  {
+    "revision": "444c0d9cf29d7ea460bff38539512cab",
+    "url": "/News-world.com/static/media/loading.444c0d9c.gif"
+  },
+  {
+    "revision": "a428a0d2936b8f909069e07535213f56",
+    "url": "/News-world.com/index.html"
+  }
+];
